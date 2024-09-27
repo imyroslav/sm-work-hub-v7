9c2056e42073e9eb4f9a090b0f376f2f04388f0b
imyroslav <imyroslav@ukr.net>
2024-09-27 13:13:09 +0700

    updates-2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,29 @@
 import data from "../officers.json";
 import OfficersList from "../OfficersList/OfficersList";
 import css from "./App.module.css"
+import { CgAirplane } from "react-icons/cg";
+import Button from "../Button/Button";
+
+const Box = ({ children }) => {
+  return (
+    <div>
+      <p>Hello {children} box!</p>
+      
+    </div>
+  )
+}
+
 
 
 export default function App() {
     return (
 
       <div className={css.container} >
+        <Box>children props here</Box>
         <h1>Available pilots</h1>
+        <CgAirplane className={css.icon} size={50} />
+        <Button>Register</Button>
+        <Button>Search</Button>
         <OfficersList officers={data}/>
       </div>
       
--- src/components/OfficerProfile/OfficerProfile.jsx
+++ src/components/OfficerProfile/OfficerProfile.jsx
@@ -1,6 +1,14 @@
-export default function OfficerProfile({ officer: { id, name, age, spec, rank, status, skills } })
+import css from "./OfficerProfile.module.css";
+import clsx from "clsx";
 
-{
+export default function OfficerProfile({ officer: { id, name, age, spec, rank, active, skills } }) {
+
+    const textClass = clsx(css.text, {
+        [css.active]: active === true,
+        [css.retired]: active === false,
+    });
+
+   
     return (
         <div>
             <p>ID: {id} </p>
@@ -8,7 +16,7 @@ export default function OfficerProfile({ officer: { id, name, age, spec, rank, s
             <p>Specialization: {spec} </p>
             <p>Age: {age}</p>
             <p>Skills: {skills} </p>
-            <p>Status; {status === "Active" ? "Is active" : "Is retired" }  </p>
+            <p className={textClass} >Status; {active === true ? "Is Active" : "Is Retired"} </p>
             <p>Rank: {rank} </p>
         </div>
 
